acdd8644f20b2f42ca2f3841e36c0d7e4014b1d2
Whizark <devaloka@whizark.com>
2015-12-20 13:15:01 +0900

    chore: add tests

---
+++ tests/Installer/InstallerTestCase.php
@@ -0,0 +1,100 @@
+<?php
+namespace Tests\Devaloka\Composer\Installer;
+
+use Composer\Composer;
+use Composer\Config;
+use Composer\Downloader\DownloadManager;
+use Composer\IO\IOInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+use Composer\Util\Filesystem;
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class InstallerTestCase
+ *
+ * @package Tests\Devaloka\Composer\Installer
+ * @author Whizark
+ */
+abstract class InstallerTestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * @var Composer
+     */
+    protected $composer;
+
+    /**
+     * @var Filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * @var Mockery\Mock|DownloadManager
+     */
+    protected $downloadManager;
+
+    /**
+     * @var Mockery\Mock|InstalledRepositoryInterface
+     */
+    protected $repository;
+
+    /**
+     * @var IOInterface
+     */
+    protected $io;
+
+    /**
+     * @var string
+     */
+    protected $vendorDir;
+
+    /**
+     * @var string
+     */
+    protected $binDir;
+
+    /**
+     * Sets up the test case.
+     */
+    protected function setUp()
+    {
+        $this->config     = new Config();
+        $this->composer   = new Composer();
+        $this->filesystem = new Filesystem();
+
+        $this->downloadManager = Mockery::mock('Composer\Downloader\DownloadManager');
+        $this->repository      = Mockery::mock('Composer\Repository\InstalledRepositoryInterface');
+        $this->io              = Mockery::mock('Composer\IO\IOInterface');
+
+        $this->vendorDir = realpath(sys_get_temp_dir()) . DIRECTORY_SEPARATOR . 'baton-test-vendor';
+        $this->binDir    = realpath(sys_get_temp_dir()) . DIRECTORY_SEPARATOR . 'baton-test-bin';
+
+        $this->config->merge(
+            array(
+                'config' => array(
+                    'vendor-dir' => $this->vendorDir,
+                    'bin-dir'    => $this->binDir,
+                ),
+            )
+        );
+        $this->composer->setConfig($this->config);
+        $this->composer->setDownloadManager($this->downloadManager);
+
+        $this->filesystem->emptyDirectory($this->vendorDir);
+        $this->filesystem->emptyDirectory($this->binDir);
+    }
+
+    /**
+     * Tears down the test case.
+     */
+    protected function tearDown()
+    {
+        $this->filesystem->removeDirectory($this->vendorDir);
+        $this->filesystem->removeDirectory($this->binDir);
+    }
+}
---
+++ tests/Installer/MuPluginInstallerTest.php
@@ -0,0 +1,569 @@
+<?php
+namespace Tests\Devaloka\Composer\Installer;
+
+use Composer\Package\Package;
+use Composer\Package\RootPackage;
+use Devaloka\Composer\Installer\MuPluginInstaller;
+use Mockery;
+
+/**
+ * Class InstallerTest
+ *
+ * @package Tests\Devaloka\Composer\Installer
+ * @author Whizark
+ */
+class InstallerInstallerTest extends InstallerTestCase
+{
+    public function testPackageTypeShouldBeDevalokaMuplugin()
+    {
+        $this->assertSame('devaloka-muplugin', MuPluginInstaller::TYPE);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function typeDataProvider()
+    {
+        return array(
+            array('devaloka-muplugin', true),
+            array('unknown-type', false),
+        );
+    }
+
+    /**
+     * @dataProvider typeDataProvider
+     *
+     * @param string $type
+     * @param bool $supports
+     */
+    public function testInstallerShouldSupportOnlyDevalokaMuplugin($type, $supports)
+    {
+        $installer = $this->createInstaller();
+
+        $this->assertSame($supports, $installer->supports($type));
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function defaultInstallPathDataProvider()
+    {
+        return array(
+            array('package', 'wp-content/mu-plugins/package/'),
+            array('tests/package', 'wp-content/mu-plugins/package/'),
+        );
+    }
+
+    /**
+     * @dataProvider defaultInstallPathDataProvider
+     *
+     * @param string $name
+     * @param string $expectedPath
+     */
+    public function testDefaultInstallPath($name, $expectedPath)
+    {
+        $installer = $this->createInstaller();
+        $package   = $this->createMuPluginPackage($name);
+
+        $installPath = $installer->getInstallPath($package);
+
+        $this->assertSame($expectedPath, $installPath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function LoaderDefaultInstallPathDataProvider()
+    {
+        return array(
+            array('package', 'wp-content/mu-plugins/'),
+            array('tests/package', 'wp-content/mu-plugins/'),
+        );
+    }
+
+    /**
+     * @dataProvider loaderDefaultInstallPathDataProvider
+     *
+     * @param string $name
+     * @param string $expectedPath
+     */
+    public function testLoaderDefaultInstallPath($name, $expectedPath)
+    {
+        $installer = $this->createInstaller();
+        $package   = $this->createMuPluginPackage($name);
+
+        $installPath = $installer->getLoaderInstallPath($package);
+
+        $this->assertSame($expectedPath, $installPath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function loaderFileDefaultPackagePathDataProvider()
+    {
+        return array(
+            array('package', 'wp-content/mu-plugins/package/mu-plugins/package.php'),
+            array('tests/package', 'wp-content/mu-plugins/package/mu-plugins/package.php'),
+        );
+    }
+
+    /**
+     * @dataProvider loaderFileDefaultPackagePathDataProvider
+     *
+     * @param string $name
+     * @param string $expectedPath
+     */
+    public function testLoaderFileDefaultPackagePath($name, $expectedPath)
+    {
+        $installer = $this->createInstaller();
+        $package   = $this->createMuPluginPackage($name);
+
+        $packagePath = $installer->getLoaderFilePackagePath($package);
+
+        $this->assertSame($expectedPath, $packagePath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function loaderFileDefaultInstallPathDataProvider()
+    {
+        return array(
+            array('package', 'wp-content/mu-plugins/package.php'),
+            array('tests/package', 'wp-content/mu-plugins/package.php'),
+        );
+    }
+
+    /**
+     * @dataProvider loaderFileDefaultInstallPathDataProvider
+     *
+     * @param string $name
+     * @param string $expectedPath
+     */
+    public function testDefaultLoaderFileInstallPath($name, $expectedPath)
+    {
+        $installer = $this->createInstaller();
+        $package   = $this->createMuPluginPackage($name);
+
+        $packagePath = $installer->getLoaderFileInstallPath($package);
+
+        $this->assertSame($expectedPath, $packagePath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function customNameInstallPathDataProvider()
+    {
+        return array(
+            array('package', 'custom-name', 'wp-content/mu-plugins/custom-name/'),
+            array('tests/package', 'custom-name', 'wp-content/mu-plugins/custom-name/'),
+        );
+    }
+
+    /**
+     * @dataProvider customNameInstallPathDataProvider
+     *
+     * @param string $package
+     * @param string $name
+     * @param string $expectedPath
+     */
+    public function testCustomNameInstallPath($package, $name, $expectedPath)
+    {
+        $installer = $this->createInstaller();
+        $package   = $this->createMuPluginPackage($package);
+
+        $package->setExtra(
+            array(
+                'installer-name' => $name,
+            )
+        );
+
+        $installPath = $installer->getInstallPath($package);
+
+        $this->assertSame($expectedPath, $installPath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function customLoaderInstallPathDataProvider()
+    {
+        return array(
+            array('package', 'path/to/loader.php', 'wp-content/mu-plugins/'),
+            array('tests/package', 'path/to/loader.php', 'wp-content/mu-plugins/'),
+        );
+    }
+
+    /**
+     * @dataProvider customLoaderInstallPathDataProvider
+     *
+     * @param string $package
+     * @param string $loader
+     * @param string $expectedPath
+     */
+    public function testCustomLoaderInstallPath($package, $loader, $expectedPath)
+    {
+        $installer = $this->createInstaller();
+        $package   = $this->createMuPluginPackage($package);
+
+        $package->setExtra(
+            array(
+                'installer-loader' => $loader,
+            )
+        );
+
+        $installPath = $installer->getLoaderInstallPath($package);
+
+        $this->assertSame($expectedPath, $installPath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function customLoaderFilePackagePathDataProvider()
+    {
+        return array(
+            array('package', 'path/to/loader.php', 'wp-content/mu-plugins/package/path/to/loader.php'),
+            array('tests/package', 'path/to/loader.php', 'wp-content/mu-plugins/package/path/to/loader.php'),
+        );
+    }
+
+    /**
+     * @dataProvider customLoaderFilePackagePathDataProvider
+     *
+     * @param $package
+     * @param $loader
+     * @param $expectedPath
+     */
+    public function testCustomLoaderFilePackagePath($package, $loader, $expectedPath)
+    {
+        $installer = $this->createInstaller();
+        $package   = $this->createMuPluginPackage($package);
+
+        $package->setExtra(
+            array(
+                'installer-loader' => $loader,
+            )
+        );
+
+        $packagePath = $installer->getLoaderFilePackagePath($package);
+
+        $this->assertSame($expectedPath, $packagePath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function customLoaderFileInstallPathDataProvider()
+    {
+        return array(
+            array('package', 'path/to/loader.php', 'wp-content/mu-plugins/loader.php'),
+            array('tests/package', 'path/to/loader.php', 'wp-content/mu-plugins/loader.php'),
+        );
+    }
+
+    /**
+     * @dataProvider customLoaderFileInstallPathDataProvider
+     *
+     * @param string $package
+     * @param string $loader
+     * @param string $expectedPath
+     */
+    public function testCustomLoaderFileInstallPath($package, $loader, $expectedPath)
+    {
+        $installer = $this->createInstaller();
+        $package   = $this->createMuPluginPackage($package);
+
+        $package->setExtra(
+            array(
+                'installer-loader' => $loader,
+            )
+        );
+
+        $installPath = $installer->getLoaderFileInstallPath($package);
+
+        $this->assertSame($expectedPath, $installPath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function customInstallPathDataProvider()
+    {
+        return array(
+            array(
+                'package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                'package',
+                '-package/devaloka-muplugin/mu-plugins/package.php/',
+            ),
+            array(
+                'tests/package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                'tests/package',
+                'tests-package/devaloka-muplugin/mu-plugins/package.php/',
+            ),
+            array(
+                'package',
+                '{$vendor}-{$name}/{$type}/{$loader}',
+                'unknown/package',
+                'wp-content/mu-plugins/package/',
+            ),
+            array(
+                'tests/package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                'unknown/package',
+                'wp-content/mu-plugins/package/',
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider customInstallPathDataProvider
+     *
+     * @param string $name
+     * @param string $path
+     * @param string $target
+     * @param string $expectedPath
+     */
+    public function testCustomInstallPath($name, $path, $target, $expectedPath)
+    {
+        $installer       = $this->createInstaller();
+        $package         = $this->createMuPluginPackage($name);
+        $consumerPackage = new RootPackage('root/package', '1.0.0', '1.0.0');
+
+        $this->composer->setPackage($consumerPackage);
+        $consumerPackage->setExtra(
+            array(
+                'installer-paths' => array(
+                    $path => array(
+                        $target,
+                    ),
+                ),
+            )
+        );
+
+        $installPath = $installer->getInstallPath($package);
+
+        $this->assertSame($expectedPath, $installPath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function LoaderCustomInstallPathDataProvider()
+    {
+        return array(
+            array(
+                'package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                'package',
+                '-package/devaloka-muplugin/mu-plugins/package.php/',
+            ),
+            array(
+                'tests/package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                'tests/package',
+                'tests-package/devaloka-muplugin/mu-plugins/package.php/',
+            ),
+            array(
+                'package',
+                '{$vendor}-{$name}/{$type}/{$loader}',
+                'unknown/package',
+                'wp-content/mu-plugins/',
+            ),
+            array(
+                'tests/package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                'unknown/package',
+                'wp-content/mu-plugins/',
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider LoaderCustomInstallPathDataProvider
+     *
+     * @param string $name
+     * @param string $path
+     * @param string $target
+     * @param string $expectedPath
+     */
+    public function testLoaderCustomInstallPath($name, $path, $target, $expectedPath)
+    {
+        $installer       = $this->createInstaller();
+        $package         = $this->createMuPluginPackage($name);
+        $consumerPackage = new RootPackage('root/package', '1.0.0', '1.0.0');
+
+        $this->composer->setPackage($consumerPackage);
+        $consumerPackage->setExtra(
+            array(
+                'installer-loader-paths' => array(
+                    $path => array(
+                        $target,
+                    ),
+                ),
+            )
+        );
+
+        $installPath = $installer->getLoaderInstallPath($package);
+
+        $this->assertSame($expectedPath, $installPath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function customInstallPathByTypeDataProvider()
+    {
+        return array(
+            array(
+                'package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                MuPluginInstaller::TYPE,
+                '-package/devaloka-muplugin/mu-plugins/package.php/',
+            ),
+            array(
+                'tests/package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                MuPluginInstaller::TYPE,
+                'tests-package/devaloka-muplugin/mu-plugins/package.php/',
+            ),
+            array(
+                'package',
+                '{$vendor}-{$name}/{$type}/{$loader}',
+                'unknown-type',
+                'wp-content/mu-plugins/package/',
+            ),
+            array(
+                'tests/package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                'unknown-type',
+                'wp-content/mu-plugins/package/',
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider customInstallPathByTypeDataProvider
+     *
+     * @param string $name
+     * @param string $path
+     * @param string $type
+     * @param string $expectedPath
+     */
+    public function testCustomInstallPathByType($name, $path, $type, $expectedPath)
+    {
+        $installer       = $this->createInstaller();
+        $package         = $this->createMuPluginPackage($name);
+        $consumerPackage = new RootPackage('root/package', '1.0.0', '1.0.0');
+
+        $this->composer->setPackage($consumerPackage);
+        $consumerPackage->setExtra(
+            array(
+                'installer-paths' => array(
+                    $path => array(
+                        'type:' . $type,
+                    ),
+                ),
+            )
+        );
+
+        $installPath = $installer->getInstallPath($package);
+
+        $this->assertSame($expectedPath, $installPath);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function loaderCustomInstallPathByTypeDataProvider()
+    {
+        return array(
+            array(
+                'package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                MuPluginInstaller::TYPE,
+                '-package/devaloka-muplugin/mu-plugins/package.php/',
+            ),
+            array(
+                'tests/package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                MuPluginInstaller::TYPE,
+                'tests-package/devaloka-muplugin/mu-plugins/package.php/',
+            ),
+            array(
+                'package',
+                '{$vendor}-{$name}/{$type}/{$loader}',
+                'unknown-type',
+                'wp-content/mu-plugins/',
+            ),
+            array(
+                'tests/package',
+                '{$vendor}-{$name}/{$type}/{$loader}/',
+                'unknown-type',
+                'wp-content/mu-plugins/',
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider loaderCustomInstallPathByTypeDataProvider
+     *
+     * @param string $name
+     * @param string $path
+     * @param string $type
+     * @param string $expectedPath
+     */
+    public function testLoaderCustomInstallPathByType($name, $path, $type, $expectedPath)
+    {
+        $installer       = $this->createInstaller();
+        $package         = $this->createMuPluginPackage($name);
+        $consumerPackage = new RootPackage('root/package', '1.0.0', '1.0.0');
+
+        $this->composer->setPackage($consumerPackage);
+        $consumerPackage->setExtra(
+            array(
+                'installer-loader-paths' => array(
+                    $path => array(
+                        'type:' . $type,
+                    ),
+                ),
+            )
+        );
+
+        $installPath = $installer->getLoaderInstallPath($package);
+
+        $this->assertEquals($expectedPath, $installPath);
+    }
+
+    /**
+     * Creates a MU plugin installer.
+     *
+     * @return MuPluginInstaller
+     */
+    protected function createInstaller()
+    {
+        $installer = new MuPluginInstaller($this->io, $this->composer);
+
+        return $installer;
+    }
+
+    /**
+     * Creates a MU plugin Package.
+     *
+     * @param string $name
+     *
+     * @return Package
+     */
+    protected function createMuPluginPackage($name)
+    {
+        $package = new Package($name, '1.0.0.0', '1.0.0');
+
+        $package->setType(MuPluginInstaller::TYPE);
+
+        return $package;
+    }
+}
